9c3ade037b2cb19e86688f3d6e6a60045e3a1733
Maciej <maprz2017@gmail.com>
2025-10-18 01:22:05 +0200

    Added camera rotation aroun the focal point

---
+++ KeroKero/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    float targetRotation;
+    float speed = 400f;
+    int direction = 0;
+    bool isMoving = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInputs();
+
+        // Move camera
+        if (isMoving)
+        {
+            transform.Rotate(Vector3.up, direction * speed * Time.deltaTime);
+            targetRotation -= speed * Time.deltaTime;
+
+            if (targetRotation < 0)
+            {
+                transform.eulerAngles += new Vector3(0, direction * targetRotation, 0);
+                isMoving = false;
+            }
+        }
+    }
+
+    void HandleInputs() 
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && !isMoving)
+        {
+            direction = 1;
+            targetRotation = 90;
+            isMoving = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && !isMoving)
+        {
+            direction = -1;
+            targetRotation = 90;
+            isMoving = true;
+        }
+    }
+}
