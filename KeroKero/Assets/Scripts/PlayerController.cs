226db8c13d682fc81902fad7e69344f23ffb91a0
Maciej <maprz2017@gmail.com>
2025-10-20 14:26:47 +0200

    Refactored the code and made jumping work smoothly

--- KeroKero/Assets/Scripts/PlayerController.cs
+++ KeroKero/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,9 +21,14 @@ public class PlayerController : MonoBehaviour
     float timeToPressBounce = 0.5f;
     bool isMoving = false;
     bool isJumping = false;
-    bool isOnWall = false;
+    int isOnWall = 0;
     bool resetMovement = false;
     bool canBounce = false;
+    bool isBouncing = false;
+
+    //=====================================================================================================
+    // Start and Update
+    //=====================================================================================================
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,61 +42,80 @@ public class PlayerController : MonoBehaviour
         HandleInputs();
     }
 
+    //=====================================================================================================
+    // Input handling
+    //=====================================================================================================
     void HandleInputs()
     {
         if (Input.GetKeyDown(KeyCode.W) && !isMoving && !isJumping)
         {
             RotatePlayer("forward");
-            StartCoroutine(MovePlayer());
+            StartCoroutine(MovePlayerOnGrid());
         }
 
         if (Input.GetKeyDown(KeyCode.S) && !isMoving && !isJumping)
         {
             RotatePlayer("back");
-            StartCoroutine(MovePlayer());
+            StartCoroutine(MovePlayerOnGrid());
         }
 
         if (Input.GetKeyDown(KeyCode.A) && !isMoving && !isJumping)
         {
             RotatePlayer("left");
-            StartCoroutine(MovePlayer());
+            StartCoroutine(MovePlayerOnGrid());
         }
 
         if (Input.GetKeyDown(KeyCode.D) && !isMoving && !isJumping)
         {
             RotatePlayer("right");
-            StartCoroutine(MovePlayer());
+            StartCoroutine(MovePlayerOnGrid());
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && !isMoving)
         {
-            if (!isJumping) { StartCoroutine(JumpUp()); }
-            if (canBounce) { StartCoroutine(BounceUp()); }
+            if (!isJumping) { JumpUp(); }
+            if (canBounce) { BounceUp(); }
         }
     }
 
+    //=====================================================================================================
+    // Unity methods
+    //=====================================================================================================
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Block"))
         {
             if (isMoving) { resetMovement = true; }
-            if (isJumping) { isOnWall = true; }
+            if (isJumping) { isOnWall++; }
         }
     }
 
-    void RotatePlayer(string direction)
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Block") && isJumping && isOnWall > 0)
+        {
+            isOnWall--;
+        }
+    }
+
+    //=====================================================================================================
+    // Custom methods
+    //=====================================================================================================
+    void RotatePlayer(string direction) // Rotate player in direction of movement
     {
         transform.eulerAngles = new Vector3(0, directions[direction], 0);
     }
 
-    IEnumerator MovePlayer()
+    IEnumerator MovePlayerOnGrid() // Move player on the 2D grid
     {
-        isMoving = true;
-        float elapsedTime = 0f;
+        isMoving = true; // Mark movement
+        float elapsedTime = 0f; // Start counting time
 
+        // Calculate positions
         originalPosition = transform.position;
         targetPosition = originalPosition + transform.forward;
 
+        // Lerp to the target position
         while (elapsedTime < timeToMove && !resetMovement)
         {
             transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / timeToMove);
@@ -98,135 +123,103 @@ public class PlayerController : MonoBehaviour
             yield return null;
         }
 
+        
         if (resetMovement)
         {
+            // Snap to the starting position if movment is reset
             transform.position = originalPosition;
             resetMovement = false;
         }
-        else { transform.position = targetPosition; }
+        else { transform.position = targetPosition; } // Snap into the position in case something is off
 
-        isMoving = false;
+        isMoving = false; // Unmark movement
     }
 
-    IEnumerator JumpUp()
+    // Move and rotate player in 3D space 
+    IEnumerator MovePlayerIn3D(Vector3 targetPositionShift, Vector3 targetRotationShift, float timeForMovement, Action afterwork)
     {
-        isJumping = true;
-        float elapsedTime = 0f;
+        float elapsedTime = 0f; // Start counting time
 
+        // Calculate positions
         originalPosition = transform.position;
-        targetPosition = originalPosition + transform.forward * 0.4f + transform.up * 1.25f;
+        targetPosition = originalPosition + targetPositionShift;
 
+        // Calculate rotations
         originalRotation = transform.eulerAngles;
-        targetRotation = originalRotation + new Vector3(-90, 0, 0);
+        targetRotation = originalRotation + targetRotationShift;
 
-        while (elapsedTime < timeToJump)
+        // Lerp to the target position and rotation
+        while (elapsedTime < timeForMovement)
         {
-            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / timeToJump);
-            transform.eulerAngles = Vector3.Lerp(originalRotation, targetRotation, elapsedTime / timeToJump);
-            elapsedTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / timeForMovement);
+            transform.eulerAngles = Vector3.Lerp(originalRotation, targetRotation, elapsedTime / timeForMovement);
+            elapsedTime += Time.deltaTime; // Count time
             yield return null;
         }
 
+        // Snap into the position and rotation in case something is off
         transform.position = targetPosition;
         transform.eulerAngles = targetRotation;
 
-        if (isOnWall) { StartCoroutine(AwaitBounce()); }
-        else { StartCoroutine(FallForwad()); }
+        afterwork(); // Do whatever needs to be done after getting to the position
     }
 
+    void JumpUp() // Jump up forward
+    {
+        isJumping = true;
+        StartCoroutine(MovePlayerIn3D(transform.forward * 0.35f + transform.up * 1.35f, new Vector3(-90, 0, 0), timeToJump, () =>
+        {
+            if (isOnWall > 0) { StartCoroutine(AwaitBounce()); }
+            else { FallForwad(); }
+        }));
+    }
+
+    void BounceUp() // Bounce from the wall in the other direction
+    {
+        canBounce = false;
+        isBouncing = true;
+        StopAllCoroutines();
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+        
+        StartCoroutine(MovePlayerIn3D(transform.forward + transform.up * 0.7f, new Vector3(0, 180, 0), timeToJump, () =>
+        {
+            isBouncing = false;
+            if (isOnWall > 0) { StartCoroutine(AwaitBounce()); }
+            else { FallForwad(); }
+        }));
+    }
+
+    void FallForwad() // Finish the jump by falling onto the grid
+    {
+        StartCoroutine(MovePlayerIn3D(-transform.up * 0.65f, new Vector3(90, 0, 0), timeToJump, () => { isJumping = false; }));
+    }
+
+    void FallBack() // Return to the ground if player doen't bounce
+    {
+        StartCoroutine(MovePlayerIn3D(transform.up * 0.35f, new Vector3(90, 0, 0), timeToJump, () => { isJumping = false; }));
+    }
+
+    // Wait for bounce input
     IEnumerator AwaitBounce()
     {
-        canBounce = true;
+        canBounce = true; // Allow bouncing
+
+        // Stop player on wall
         gameObject.GetComponent<Rigidbody>().useGravity = false;
         gameObject.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
 
-        yield return new WaitForSeconds(timeToPressBounce);
+        yield return new WaitForSeconds(timeToPressBounce); // Waiting for input
 
-        gameObject.GetComponent<Rigidbody>().useGravity = true;
-        isOnWall = false;
+        gameObject.GetComponent<Rigidbody>().useGravity = true; // Turn the gravity back on
 
-        if (canBounce)
+        if (canBounce && !isBouncing)
         {
-            canBounce = false;
-            StartCoroutine(FallBack());
+            canBounce = false; // Disable bouncing
+            FallBack(); // Return to the ground
         }
     }
 
-    IEnumerator BounceUp()
-    {
-        canBounce = false;
-        gameObject.GetComponent<Rigidbody>().useGravity = true;
-        float elapsedTime = 0f;
-
-        originalPosition = transform.position;
-        targetPosition = originalPosition + transform.forward + transform.up * 1.4f;
-
-        originalRotation = transform.eulerAngles;
-        targetRotation = originalRotation + new Vector3(90, 180, 0);
-
-        while (elapsedTime < timeToJump)
-        {
-            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / timeToJump);
-            transform.eulerAngles = Vector3.Lerp(originalRotation, targetRotation, elapsedTime / timeToJump);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        transform.position = targetPosition;
-        transform.eulerAngles = targetRotation;
-
-        isJumping = false;
-    }
-
-    IEnumerator FallForwad()
-    {
-        float elapsedTime = 0f;
-
-        originalPosition = transform.position;
-        targetPosition = originalPosition + -transform.up * 0.6f;
-
-        originalRotation = transform.eulerAngles;
-        targetRotation = originalRotation + new Vector3(90, 0, 0);
-
-        while (elapsedTime < timeToJump)
-        {
-            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / timeToJump);
-            transform.eulerAngles = Vector3.Lerp(originalRotation, targetRotation, elapsedTime / timeToJump);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        transform.position = targetPosition;
-        transform.eulerAngles = targetRotation;
-
-        isJumping = false;
-    }
-
-    IEnumerator FallBack()
-    {
-        float elapsedTime = 0f;
-
-        originalPosition = transform.position;
-        targetPosition = originalPosition + transform.up * 0.4f;
-
-        originalRotation = transform.eulerAngles;
-        targetRotation = originalRotation + new Vector3(90, 0, 0);
-
-        while (elapsedTime < timeToJump)
-        {
-            transform.position = Vector3.Lerp(originalPosition, targetPosition, elapsedTime / timeToJump);
-            transform.eulerAngles = Vector3.Lerp(originalRotation, targetRotation, elapsedTime / timeToJump);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        transform.position = targetPosition;
-        transform.eulerAngles = targetRotation;
-
-        isJumping = false;
-    }
-
-    public void RotateDirections(float angle)
+    public void RotateDirections(float angle) // Rotate movement angles relative to the camera movement
     {
         directions["forward"] += angle;
         directions["left"] += angle;
