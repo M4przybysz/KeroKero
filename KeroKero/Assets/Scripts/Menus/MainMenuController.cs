9f4054fb89be64a5da0157b631325bccc5fe69aa
Maciej <maprz2017@gmail.com>
2025-10-29 18:10:57 +0100

    Refactored main menu code and added pause menu to all 3 levels

--- KeroKero/Assets/Scripts/Menus/MainMenuController.cs
+++ KeroKero/Assets/Scripts/Menus/MainMenuController.cs
@@ -11,17 +11,19 @@ public class MainMenuController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Unlock and show cursor
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
 
-    public void PlayLevel1() { SceneManager.LoadScene("Level1"); }
+    public void PlayLevel(string levelName)
+    {
+        // Lock and hide cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 
-    public void PlayLevel2() { SceneManager.LoadScene("Level2"); }
-
-    public void PlayLevel3() { SceneManager.LoadScene("Level3"); }
-    
-    public void PlayEndlessMode() { SceneManager.LoadScene("EndlessMode"); }
+        SceneManager.LoadScene(levelName); // Load level
+    }
 
     public void QuitGame()
     {
