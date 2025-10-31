b40b19e9df7d92e01fe696e90f819537fffe2f3f
Maciej <maprz2017@gmail.com>
2025-10-31 17:48:00 +0100

    Added UI showing frog's climb progress

---
+++ KeroKero/Assets/Scripts/UIController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] RectTransform playerMarker, progressBar, holeHeightMarker;
+    [SerializeField] float levelHeight;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        playerMarker.position = new Vector3(
+            playerMarker.position.x,
+            140 + (player.position.y - 1.15f) * holeHeightMarker.sizeDelta.y / levelHeight);
+
+        progressBar.sizeDelta = new Vector2(
+            progressBar.sizeDelta.x,
+            (player.position.y - 1.15f) * holeHeightMarker.sizeDelta.y / levelHeight);
+    }
+}
